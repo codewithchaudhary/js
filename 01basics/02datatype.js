ab139886bf132671acfb1dc6c5bfcf9c08ac53ad
codewithchaudhary <codewithchaudhary@gmail.com>
2024-07-19 11:54:47 +0000

    add datatype and rename

---
+++ 01basics/02datatype.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as newer version
+
+
+//alert("hello") // we are useinging node js not browser
+// code readibality should be high
+
+console.log("chaudhary")
+
+
+
+
+let name = "viraj chaudhary"
+let age  = 22
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// bollen => true/false
+// null => standalone value (represent the empty)
+// undefined => value not assigned yet
+// symbol => unique
+
+
+
+//object
+
+console.log(typeof "viraj")
+console.log(typeof null)
+console.log(typeof undefined)
