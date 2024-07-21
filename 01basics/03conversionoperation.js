042053c8dc511efbd609411ddaca1a7735db4b0f
codewithchaudhary <codewithchaudhary@gmail.com>
2024-07-21 08:25:26 +0000

    comparision js

--- 01basics/03conversionoperation.js
+++ 01basics/03conversionoperation.js
@@ -2,13 +2,13 @@
 // let score = 33
 // let score = "33"
 // let score = "33abc"
-let score = "33abc"
-console.log(typeof score);
-console.log(typeof(score));
-let valueInNumber = Number(score);
-console.log(typeof(valueInNumber));
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);
+// let score = "33abc"
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
 
 
 // "33" => 33
@@ -18,17 +18,62 @@ console.log(valueInNumber);
 // let isLogedIn =  1
 // let isLogedIn =  0
 // let isLogedIn =  ""
-let isLogedIn =  "viraj"
-let bolleanIsisLogedIn = Boolean(isLogedIn);
-console.log(bolleanIsisLogedIn);
+// let isLogedIn =  "viraj"
+// let bolleanIsisLogedIn = Boolean(isLogedIn);
+// console.log(bolleanIsisLogedIn);
 
 
-// 1 => true; 0=> false
-// "" => false
-// "viraj" => true
+// // 1 => true; 0=> false
+// // "" => false
+// // "viraj" => true
 
 
-let someNumber = 33
-let stringsomeNumber = String(someNumber);
+// let someNumber = 33
+// let stringsomeNumber = String(someNumber);
 
-console.table([someNumber,stringsomeNumber,typeof someNumber, typeof stringsomeNumber ]);
+// console.table([someNumber,stringsomeNumber,typeof someNumber, typeof stringsomeNumber ]);
+
+
+
+// ------------------------------ OPERATIONS ---------------------------
+
+// let value =  3
+// let negvalue = -value
+// console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2%2);
+
+
+
+// let str1 = "hello"
+// let str2 = " viraj"
+// let str3 = str1 + str2;
+// console.log(str3);
+
+
+
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+
+// console.log(+true)
+// console.log(+"")
+
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2
+
+
+let gamecounter = 100
+gamecounter++
+console.log(gamecounter)
